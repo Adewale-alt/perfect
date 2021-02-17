9860c929b0264cf7692625c100dbc76369bdcd35
Adewale-alt <78614131+Adewale-alt@users.noreply.github.com>
2021-02-17 02:00:43 +0100

    Add files via upload

---
+++ befit.js
@@ -0,0 +1,88 @@
+let likebtn = document.querySelector('.input')
+let input = document.querySelector('.input2')
+likebtn.addEventListener('click', ()=> {
+  input.value = parseInt(input.value) + 1;
+})
+
+
+let likebtna = document.querySelector('.inputb')
+let inputa = document.querySelector('.input3')
+likebtna.addEventListener('click', ()=> {
+  inputa.value = parseInt(inputa.value) + 1;
+})
+
+
+
+
+let likebtnb = document.querySelector('.inputc')
+let inputb = document.querySelector('.input4')
+likebtnb.addEventListener('click', ()=> {
+  inputb.value = parseInt(inputb.value) + 1;
+})
+
+
+
+let likebtnc = document.querySelector('.inputd')
+let inputc = document.querySelector('.input5')
+likebtnc.addEventListener('click', ()=> {
+  inputc.value = parseInt(inputc.value) + 1;
+})
+
+
+
+
+let likebtnd = document.querySelector('.inpute')
+let inputd = document.querySelector('.input6')
+likebtnd.addEventListener('click', ()=> {
+  inputd.value = parseInt(inputd.value) + 1;
+})
+
+
+
+
+let likebtne = document.querySelector('.inputf')
+let inpute = document.querySelector('.input7')
+likebtne.addEventListener('click', ()=> {
+  inpute.value = parseInt(inpute.value) + 1;
+})
+
+
+
+let likebtnf = document.querySelector('.inputg')
+let inputf = document.querySelector('.input8')
+likebtnf.addEventListener('click', ()=> {
+  inputf.value = parseInt(inputf.value) + 1;
+})
+
+
+
+let likebtng = document.querySelector('.inputh')
+let inputg = document.querySelector('.input9')
+likebtng.addEventListener('click', ()=> {
+  inputg.value = parseInt(inputg.value) + 1;
+})
+
+
+
+let likebtnh = document.querySelector('.inputi')
+let inputh = document.querySelector('.input10')
+likebtnh.addEventListener('click', ()=> {
+  inputh.value = parseInt(inputh.value) + 1;
+})
+
+
+function code(){
+    var myText = "Registration Sucessfull";
+    alert(myText);
+        
+
+}
+
+
+
+function whatsapp(){
+    var myNum = "+2348177618354";
+    alert(myNum);
+        
+
+}
